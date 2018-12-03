f1619f0f14f1a99e7990a28899f233840304de52
Raymond Dai <Raymond Dai>
2018-12-03 15:49:30 +0800

    fix some route issues

--- fame-front/pages/accel/index.vue
+++ fame-front/pages/accel/index.vue
@@ -1,5 +1,6 @@
 <template>
   <div>
+    <nuxt-link class="Home" :to="{ path: '/status' }">Status Home</nuxt-link>
     <p> This is an accel begin </p>
     <p> {{ $store.state.acc.data }} </p>
     <p> This is an accel end </p>
