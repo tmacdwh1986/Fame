56fe91368b599ecdbd13f536dd55880bb765986e
YangXia <xyang101@ford.com>
2018-12-25 16:36:22 +0800

    刪除无用的文件

--- fame-server/src/main/java/com/zbw/fame/controller/HomeController.java
+++ fame-server/src/main/java/com/zbw/fame/controller/HomeController.java
@@ -11,23 +11,18 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.util.StringUtils;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.github.pagehelper.Page;
 import com.zbw.fame.dto.Archives;
-import com.zbw.fame.dto.CommentDto;
 import com.zbw.fame.dto.MetaDto;
 import com.zbw.fame.dto.Pagination;
 import com.zbw.fame.model.Articles;
-import com.zbw.fame.model.Comments;
 import com.zbw.fame.service.ArticlesService;
-import com.zbw.fame.service.CommentsService;
 import com.zbw.fame.service.DayDistanceService;
 import com.zbw.fame.service.EmailService;
 import com.zbw.fame.service.MetasService;
@@ -54,9 +49,6 @@ public class HomeController extends BaseController {
 
 	@Autowired
 	private MetasService metasService;
-
-	@Autowired
-	private CommentsService commentsService;
 
 	@Autowired
 	private EmailService emailService;
@@ -195,84 +187,6 @@ public class HomeController extends BaseController {
 		}
 		transformContent(page);
 		return RestResponse.ok(page);
-	}
-
-	/**
-	 * 获取文章的评论
-	 *
-	 * @param articleId
-	 *            文章id
-	 * @param page
-	 *            第几页
-	 * @param limit
-	 *            每页数量
-	 * @return {@see Pagination<Comments>}
-	 */
-	@GetMapping("comment")
-	public RestResponse getArticleComment(@RequestParam Integer articleId,
-			@RequestParam(required = false, defaultValue = "1") Integer page,
-			@RequestParam(required = false, defaultValue = FameConsts.PAGE_SIZE) Integer limit) {
-		Page<Comments> comments = commentsService.getCommentsByArticleId(articleId, page, limit);
-		for (Comments comment : comments) {
-			comment.setContent(FameUtil.mdToHtml(comment.getContent()));
-		}
-		return RestResponse.ok(new Pagination<Comments>(comments));
-	}
-
-	/**
-	 * 发表评论
-	 *
-	 * @param articleId
-	 *            文章id
-	 * @param pId
-	 *            父评论id
-	 * @param content
-	 *            评论内容
-	 * @param name
-	 *            评论用户名
-	 * @param email
-	 *            评论用户email
-	 * @param website
-	 *            评论用户网址
-	 * @return {@see RestResponse.ok()}
-	 */
-	@PostMapping("comment")
-	public RestResponse postComment(@RequestParam Integer articleId, @RequestParam(required = false) Integer pId,
-			@RequestParam String content, @RequestParam String name, @RequestParam(required = false) String email,
-			@RequestParam(required = false) String website) {
-		Comments comments = new Comments();
-		comments.setArticleId(articleId);
-		comments.setPId(pId);
-		comments.setContent(content);
-		comments.setName(name);
-		comments.setEmail(email);
-		comments.setWebsite(website);
-		comments.setIp(FameUtil.getIp());
-		comments.setAgent(FameUtil.getAgent());
-		commentsService.save(comments);
-
-		// 发送邮件提醒
-		CommentDto commentDetail = commentsService.getCommentDetail(comments.getId());
-		emailService.sendEmailToAdmin(commentDetail);
-		if (null != commentDetail.getPComment() && !StringUtils.isEmpty(commentDetail.getPComment().getEmail())) {
-			emailService.sendEmailToUser(commentDetail, commentDetail.getPComment().getEmail());
-		}
-		return RestResponse.ok();
-	}
-
-	/**
-	 * 顶或踩评论
-	 *
-	 * @param commentId
-	 *            评论id
-	 * @param assess
-	 *            {@link Types#AGREE},{@link Types#DISAGREE}
-	 * @return {@see RestResponse.ok()}
-	 */
-	@PostMapping("comment/{commentId}/assess")
-	public RestResponse assessComment(@PathVariable Integer commentId, @RequestParam String assess) {
-		commentsService.assessComment(commentId, assess);
-		return RestResponse.ok();
 	}
 
 	/**
